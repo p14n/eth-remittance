b7c285f0a45c61d46ccef50ffc167dd020868452
Dean Chapman <dean@p14n.com>
2018-04-25 17:47:52 +0100

    Goals met

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,18 +3,35 @@ pragma solidity ^0.4.17;
 contract Remittance {
 
   bytes32 hash;
+  bool killed = false;
+  address owner;
+  address recipient;
 
   event PaidEvent(uint amount,address recipient);
+  event Instantiated(bytes32 hash,address recipient);
+  event ToggleKill(bool killed);
 
-  function Remittance (address recipient,bytes32 password) public payable {
-    hash = keccak256(recipient,password);
+  function Remittance (address _recipient,bytes32 _hash) public payable {
+    owner = msg.sender;
+    hash = _hash;
+    recipient = _recipient;
+    Instantiated(_hash,_recipient);
   }
+  
+  function withdraw(string password) public {
 
-  function withdraw(bytes32 password) public {
+    require(!killed);
+    require(msg.sender == recipient);
     require(hash == keccak256(msg.sender,password));
+    require(this.balance > 0);
+
     uint toSend = this.balance;
     msg.sender.transfer(this.balance);
     PaidEvent(toSend,msg.sender);
-   }
-
+  }
+  function toggleKill() public {
+    require(msg.sender == owner);
+    killed = !killed;
+    ToggleKill(killed);
+  }
 }
--- test/remittance.js
+++ test/remittance.js
@@ -1,12 +1,18 @@
 require('chai').use(require('chai-as-promised')).should();
+var utils = require('web3-utils');
+var expectedExceptionPromise = require('./expected_exception_testRPC_and_geth');
+
+
 var Remittance = artifacts.require("./Remittance.sol");
 
 contract('Remittance', function ([alice,bob,carol]) {
 
     let remittance;
+    let hash;
     beforeEach('setup contract for each test', async () => {
+        hash = await utils.soliditySha3(carol,"pwd");
         remittance = await Remittance.new(carol,
-                                         "pwd",
+                                         hash,
                                           { from: alice,
                                             value: 20000})
     })
@@ -14,22 +20,25 @@ contract('Remittance', function ([alice,bob,carol]) {
     it("should allow withdrawl on correct password",async () => {
         const c_balance = await web3.eth.getBalance(carol);
         const tx = await remittance.withdraw("pwd",
-                                             { from: carol,
-                                               gasPrice: 1});
+                                             { from: carol });
         const c_new = await web3.eth.getBalance(carol);
-        assert(c_balance.plus(20000).minus(tx.receipt.gasUsed).eq(c_new))
+        let gasCost = await web3.eth.getTransaction(tx.tx)
+                .gasPrice.times(tx.receipt.gasUsed);
+
+        assert(c_balance.plus(20000).minus(gasCost).eq(c_new))
     })
 
     it("should prevent withdrawl on incorrect password",async () => {
-        await remittance.withdraw("nope",{ from: carol,
-                                           gasPrice: 1})
-            .should.be.rejectedWith("VM Exception while processing transaction: revert");;
+        await expectedExceptionPromise( () => remittance.withdraw("nope",{ from: carol }));
+
+    })
+    it("should prevent withdrawl when killed",async () => {
+        await remittance.toggleKill({ from:alice });
+        await expectedExceptionPromise( () => remittance.withdraw("pwd",{ from: carol}))
 
     })
     it("should prevent withdrawl to incorrect account",async () => {
-        await remittance.withdraw("nope",{ from: bob,
-                                           gasPrice: 1})
-            .should.be.rejectedWith("VM Exception while processing transaction: revert");;
+        await expectedExceptionPromise( () => remittance.withdraw("pwd",{ from: bob }))
 
     })
 
